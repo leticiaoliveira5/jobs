608f7fdb40df52dc922f5888af00dded51a68e3e
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-04-08 23:36:39 -0300

    a better test

--- spec/features/candidate_edits_resume_spec.rb
+++ spec/features/candidate_edits_resume_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'candidate edits resume' do
   scenario 'successfully' do
     candidate = create(:candidate)
+    resume = candidate.resume
 
     login_as candidate, scope: :candidate
     visit root_path
@@ -12,6 +13,8 @@ feature 'candidate edits resume' do
     click_on 'Atualizar currículo'
 
     expect(current_path).to eq resume_path(candidate.resume)
+    expect(page).to have_content 'Currículo atualizado com sucesso'
     expect(page).to have_content 'Segundo grau'
+    expect(resume.reload.education).to eq 'Segundo grau'
   end
 end
