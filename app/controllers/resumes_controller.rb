721e700971019a26e1141a36f80078e9734f3bf1
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-04-09 23:23:11 -0300

    Tests for resumes (features) (#19)
    * Implementa testes de feature
    
    * Adiciona cenário de segurança
    
    * details
    
    * resume now updates successfully
    
    * partial to show object errors
    
    * adds final line
    
    * puts partial to use
    
    * a better test
    
    * updates feature tests
    
    * fixes duplication
    
    * updates i18n
    
    * removes else
    
    * adds endline

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,12 @@
 class ResumesController < ApplicationController
+  before_action :authenticate_candidate!, only: %i[create edit update]
+
   def index; end
 
   def show
+    unless candidate_signed_in? || employee_signed_in?
+      redirect_to root_path, alert: 'Você não tem permissão para ver esta página'
+    end
     @resume = Resume.joins(:candidate).find(params[:id])
   end
 
@@ -16,17 +21,17 @@ class ResumesController < ApplicationController
   def update
     @resume = Resume.find(params[:id])
     @resume.update(resume_params)
-    redirect_to resume_path(@resume)
+    redirect_to resume_path(@resume), notice: t('.success') if @resume.save
   end
 
   private
 
   def resume_params
-    params.permit(:address,
-                  :education,
-                  :experience,
-                  :languages,
-                  :abilities,
-                  :courses)
+    params.require(:resume).permit(:address,
+                                   :education,
+                                   :experience,
+                                   :languages,
+                                   :abilities,
+                                   :courses)
   end
 end
