9f407ae71c50a2eed5505ca44b17ee6d3e2a4f3a
leticiaoliveira5 <leticia.oliveira@vindi.com.br>
2022-04-11 19:31:07 -0300

    test goes back to what it was

--- spec/features/candidate/candidate_edits_account_details_spec.rb
+++ spec/features/candidate/candidate_edits_account_details_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
 
 feature 'candidate edits account details' do
-  let(:candidate) { create(:candidate) }
-
   scenario 'successfully' do
-    login_as candidate, scope: :candidate
     visit root_path
+    within('.dropdown-signin') do
+      click_on 'Candidato'
+    end
+    fill_in 'Nome', with: 'Maria'
+    fill_in 'Sobrenome', with: 'Pereira'
+    fill_in 'E-mail', with: 'mariap@email.com'
+    fill_in 'Senha', with: '123456'
+    click_on 'Sign up'
     click_on 'Área do candidato'
     click_on 'Editar dados da minha conta'
     fill_in 'About me', with: 'Olá, meu nome é Maria'
     fill_in 'Current password', with: '123456'
     click_on 'Update'
 
-    expect(Candidate.first.reload.about_me).to eq('Olá, meu nome é Maria')
+    expect(Candidate.first.about_me).to eq('Olá, meu nome é Maria')
   end
 end
