73277d0bbb8935fc3cb2019bbe40e579f9ee7762
leticiaoliveira5 <leticia.oliveira@vindi.com.br>
2022-04-11 20:44:15 -0300

    fixes some test length issues

--- spec/features/employee/employee_edits_job_opportunity_spec.rb
+++ spec/features/employee/employee_edits_job_opportunity_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 feature 'employee edits job opportunity' do
-  scenario 'successfully' do
-    employee = create(:employee)
-    company = employee.company
-    job_opportunity = create(:job_opportunity, job_title: 'Dummie',
-                                               description: 'Auxiliar de eventos',
-                                               company: company)
+  let!(:employee) { create(:employee) }
+  let(:company) { employee.company }
 
+  before do
+    create(:job_opportunity, job_title: 'Dummie',
+                             description: 'Auxiliar de eventos',
+                             company: company)
+  end
+
+  scenario 'successfully' do
     login_as employee, scope: :employee
     visit root_path
     click_on 'Área da empresa'
     click_on 'Dummie'
     click_on 'Editar vaga'
     fill_in 'Descrição', with: 'Auxiliar em diversas atividades, como organização de provas'
-    fill_in 'Data limite', with: 2.months.from_now
     click_on 'Atualizar'
 
-    expect(current_path).to eq job_opportunity_path(job_opportunity)
-    expect(page).to have_content 'Globe'
-    expect(page).to have_content 'Auxiliar em diversas atividades, como organização de provas'
+    expect(current_path).to eq job_opportunity_path(JobOpportunity.last)
+    expect(JobOpportunity.last.description).to eq 'Auxiliar em diversas atividades, como organização de provas'
   end
 end
--- spec/features/employee/employee_makes_proposal_to_candidate_spec.rb
+++ spec/features/employee/employee_makes_proposal_to_candidate_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 feature 'Employee makes proposal to candidate' do
-  scenario 'successfully' do
-    # arrange
-    company = create(:company)
-    employee = create(:employee, company: company)
-    candidate = create(:candidate, firstname: 'Juliana')
-    job_opportunity = create(:job_opportunity, job_title: 'Desenvolvedor', company: company)
+  let!(:company) { create(:company) }
+  let(:employee) { create(:employee, company: company) }
+  let(:job_opportunity) { create(:job_opportunity, job_title: 'Desenvolvedor', company: company) }
+
+  before do
+    create(:candidate, firstname: 'Juliana')
     create(:job_application, candidate: candidate, job_opportunity: job_opportunity)
-    # act
-    login_as employee
+  end
+
+  scenario 'successfully' do
+    login_as employee, scope: :employee
     visit root_path
     click_on 'Área da empresa'
     click_on 'Desenvolvedor'
@@ -18,7 +20,7 @@ feature 'Employee makes proposal to candidate' do
     fill_in 'Proposta salarial', with: '2500'
     fill_in 'Data de início', with: '25/04/2021'
     click_on 'Enviar'
-    # assert
+
     expect(JobProposal.count).to eq(1)
     expect(page).to have_content 'A proposta foi enviada para o candidato'
   end
