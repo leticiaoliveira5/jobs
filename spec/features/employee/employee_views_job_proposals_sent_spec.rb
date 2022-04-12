6ce6a1b8eb1e6ed548a0dcef56f4417906f513d7
leticiaoliveira5 <leticia.oliveira@vindi.com.br>
2022-04-11 21:28:22 -0300

    job_application

--- spec/features/employee/employee_views_job_proposals_sent_spec.rb
+++ spec/features/employee/employee_views_job_proposals_sent_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 feature 'Employee views job proposals sent' do
   let!(:employee) { create(:employee) }
-  let(:candidate) { create(:candidate, firstname: 'Juliana', surname: 'Fagundes') }
-  let(:job_opportunity) do
+  let!(:candidate) { create(:candidate, firstname: 'Juliana', surname: 'Fagundes') }
+  let!(:job_opportunity) do
     create(:job_opportunity, job_title: 'Desenvolvedor',
                              company: employee.company)
+  end
+  let(:job_application) do
+    create(:job_application, candidate: candidate, job_opportunity: job_opportunity)
   end
 
   before do
