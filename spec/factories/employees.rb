0ccd42785e78c5dd713bdf5a3ac4977dc989e0ac
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-04-19 20:51:44 -0300

    uses faker for factories

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :employee do
-    firstname { 'Fausto' }
-    surname { 'Silva' }
+    firstname { Faker::Name.first_name }
+    surname { Faker::Name.last_name }
     email { 'fausto@globe.com' }
     password { '123456' }
     company
