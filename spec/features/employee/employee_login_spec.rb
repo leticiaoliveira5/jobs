456b6a87c4f0c9108bb01a5a23b7320099a520b7
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-04-19 23:32:38 -0300

    better yet

--- spec/features/employee/employee_login_spec.rb
+++ spec/features/employee/employee_login_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 feature 'Employee logs in' do
   before do
     create(:employee, email: 'leticia@email.com', password: '123456')
-  end
-
-  scenario 'successfully' do
     visit root_path
     within('.dropdown-login') { click_on 'Empresa' }
     within('form') do
@@ -13,19 +10,14 @@ feature 'Employee logs in' do
       fill_in 'Senha', with: '123456'
       click_on 'Log in'
     end
+  end
 
+  scenario 'successfully' do
     expect(page).to have_content 'leticia@email.com'
     expect(page).to have_link 'Sair'
   end
 
   scenario 'and logs out' do
-    visit root_path
-    within('.dropdown-login') { click_on 'Empresa' }
-    within('form') do
-      fill_in 'E-mail', with: 'leticia@email.com'
-      fill_in 'Senha', with: '123456'
-      click_on 'Log in'
-    end
     click_on 'Sair'
 
     expect(page).not_to have_content 'leticia@email.com'
