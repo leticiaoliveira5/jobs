72d52de679303b4b038c9aa476ae48a431670fe7
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-04-19 21:08:26 -0300

    reorganize

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -2,10 +2,10 @@ require 'faker'
 
 FactoryBot.define do
   factory :employee do
+    company
     firstname { Faker::Name.first_name }
     surname { Faker::Name.last_name }
     email { 'fausto@globe.com' }
     password { '123456' }
-    company
   end
 end
