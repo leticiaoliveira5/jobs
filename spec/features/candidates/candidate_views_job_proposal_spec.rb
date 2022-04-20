f6891ac46809921393b579930761a321c97eab77
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-04-19 22:11:37 -0300

    fixes similar blocks of code

--- spec/features/candidates/candidate_views_job_proposal_spec.rb
+++ spec/features/candidates/candidate_views_job_proposal_spec.rb
@@ -23,31 +23,32 @@ feature 'Candidate views job proposals' do
     expect(page).to have_link 'Desenvolvedor - Globe'
   end
 
-  scenario 'and accepts it' do
-    login_as candidate, scope: :candidate
-    visit root_path
-    click_on 'Área do candidato'
-    click_on 'Desenvolvedor - Globe'
-    within('.accept-form') do
-      fill_in 'start_date_confirmation', with: '01/01/2022'
-      click_on 'Confirmar'
-    end
-    # assert
-    expect(page).to have_content 'Confirmação enviada com sucesso!'
-    expect(JobProposal.first.status).to eq('accepted')
-  end
-
-  scenario 'and rejects it' do
-    login_as candidate, scope: :candidate
-    visit root_path
-    click_on 'Área do candidato'
-    click_on 'Desenvolvedor - Globe'
-    within('.reject-form') do
-      fill_in 'rejection_motive', with: 'motivo teste'
-      click_on 'Confirmar'
+  feature 'Candidate responds to job proposal' do
+    before do
+      login_as candidate, scope: :candidate
+      visit root_path
+      click_on 'Área do candidato'
+      click_on 'Desenvolvedor - Globe'
     end
 
-    expect(page).to have_content 'Resposta enviada com sucesso'
-    expect(JobProposal.first.status).to eq('rejected')
+    scenario 'accepts it' do
+      within('.accept-form') do
+        fill_in 'start_date_confirmation', with: '01/01/2022'
+        click_on 'Confirmar'
+      end
+
+      expect(page).to have_content 'Confirmação enviada com sucesso!'
+      expect(JobProposal.first.status).to eq('accepted')
+    end
+
+    scenario 'rejects it' do
+      within('.reject-form') do
+        fill_in 'rejection_motive', with: 'motivo teste'
+        click_on 'Confirmar'
+      end
+
+      expect(page).to have_content 'Resposta enviada com sucesso'
+      expect(JobProposal.first.status).to eq('rejected')
+    end
   end
 end
