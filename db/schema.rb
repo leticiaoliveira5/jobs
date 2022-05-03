8bd272d53845d3c0c8066c9953e5431c854b1efa
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-05-02 21:03:18 -0300

    update rails task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_132508) do
+ActiveRecord::Schema[6.1].define(version: 2022_04_08_132508) do
 
   create_table "candidates", force: :cascade do |t|
     t.string "email", default: "", null: false
