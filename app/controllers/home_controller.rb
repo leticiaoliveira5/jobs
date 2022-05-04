75c34d461c3578e329b25a7b7128f75b33cc8adf
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-05-04 09:40:51 -0300

    rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if candidate_signed_in? 
+    if candidate_signed_in?
       redirect_to candidates_path(current_candidate)
     elsif employee_signed_in?
       redirect_to company_path(current_employee.company)
