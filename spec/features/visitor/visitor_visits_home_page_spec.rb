33a0ec943ef870aa97f70aaeb3a587bb0ad53830
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-05-04 12:41:19 -0300

    adds test scenario for home page

--- spec/features/visitor/visitor_visits_home_page_spec.rb
+++ spec/features/visitor/visitor_visits_home_page_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 feature 'Visitor visits home page' do
+  before do
+    apple = create(:company, name: 'Apple', domain: 'apple.com')
+    create(:company, name: 'Microsoft', domain: 'microsoft.com', cnpj: '12345678911235')
+    create(:job_opportunity, company: apple, job_title: 'Desenvolvedor',
+                             job_level: 'Pleno', place: 'Home Office',
+                             status: :active)
+  end
+
   scenario 'successfully' do
     visit root_path
 
@@ -13,13 +21,18 @@ feature 'Visitor visits home page' do
   end
 
   scenario 'and views registered companies' do
-    create(:company, name: 'Apple', domain: 'apple.com')
-    create(:company, name: 'Microsoft', domain: 'microsoft.com', cnpj: '12345678911235')
-
     visit root_path
     click_on 'Ver empresas cadastradas'
 
     expect(page).to have_link('Apple')
     expect(page).to have_link('Microsoft')
+  end
+
+  scenario 'and views recent job opportunities' do
+    visit root_path
+    click_on 'Vagas recentes'
+
+    expect(page).to have_link('Desenvolvedor')
+    expect(page).to have_text('Pleno') && have_text('Home Office')
   end
 end
