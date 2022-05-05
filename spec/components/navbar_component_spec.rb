10f30866a28d0c0691f73a85ad003b73d326d46d
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-05-05 10:01:47 -0300

    test for component

--- spec/components/navbar_component_spec.rb
+++ spec/components/navbar_component_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe NavbarComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+    it "renders nav bar" do
+      rendered_component = described_class.new(current_user: nil)).to_html
+
+      expect(rendered_component).to have_content('Jobs')
+      expect(rendered_component).to have_link("Empresas cadastradas", href: '/companies')
+      expect(rendered_component).to have_link("Vagas Recentes", href: '/job_opportunities')
+    end
 end
