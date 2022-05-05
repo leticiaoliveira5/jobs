380987597439e91ab8ff2da9f1b57c4c09cae4ac
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-05-05 10:04:39 -0300

    Update spec/components/navbar_component_spec.rb

--- spec/components/navbar_component_spec.rb
+++ spec/components/navbar_component_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe NavbarComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
-
     it "renders nav bar" do
-      rendered_component = described_class.new(current_user: nil)).to_html
+      rendered_component = described_class.new(current_user: nil).to_html
 
       expect(rendered_component).to have_content('Jobs')
       expect(rendered_component).to have_link("Empresas cadastradas", href: '/companies')
