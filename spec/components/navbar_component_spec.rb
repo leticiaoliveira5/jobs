186adedbdd9337ddb9be235becf69f66f4c674bd
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-05-05 11:09:16 -0300

    component test passing :white_check_mark:

--- spec/components/navbar_component_spec.rb
+++ spec/components/navbar_component_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe NavbarComponent, type: :component do
     it "renders nav bar" do
-      rendered_component = described_class.new(current_user: nil)
+      rendered_component = render_inline(described_class.new(current_user: nil)).to_html
 
       expect(rendered_component).to have_content('Jobs')
-      expect(rendered_component).to have_link("Empresas cadastradas", href: '/companies')
-      expect(rendered_component).to have_link("Vagas Recentes", href: '/job_opportunities')
+      expect(rendered_component).to have_link("Ver empresas cadastradas", href: '/companies')
+      expect(rendered_component).to have_link("Vagas recentes", href: '/job_opportunities')
     end
 end
