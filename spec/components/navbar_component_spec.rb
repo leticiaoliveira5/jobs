4ecbd74efb0859d9818da1a67973b1b27c44110d
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-05-05 11:11:00 -0300

    rubocop

--- spec/components/navbar_component_spec.rb
+++ spec/components/navbar_component_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe NavbarComponent, type: :component do
-    it "renders nav bar" do
-      rendered_component = render_inline(described_class.new(current_user: nil)).to_html
+  it 'renders nav bar' do
+    rendered_component = render_inline(described_class.new(current_user: nil)).to_html
 
-      expect(rendered_component).to have_content('Jobs')
-      expect(rendered_component).to have_link("Ver empresas cadastradas", href: '/companies')
-      expect(rendered_component).to have_link("Vagas recentes", href: '/job_opportunities')
-    end
+    expect(rendered_component).to have_content('Jobs')
+    expect(rendered_component).to have_link('Ver empresas cadastradas', href: '/companies')
+    expect(rendered_component).to have_link('Vagas recentes', href: '/job_opportunities')
+  end
 end
