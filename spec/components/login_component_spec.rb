f52cd01191c92e29797cf7063adcf46fda6631a9
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-22 13:40:26 -0300

    newline

--- spec/components/login_component_spec.rb
+++ spec/components/login_component_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe LoginComponent, type: :component do
   let(:rendered_component) { render_inline(described_class.new).to_html }
+
   it 'renders the login component' do
     expect(rendered_component).to have_css('.tabs')
     expect(rendered_component).to have_css('.tabcontent')
