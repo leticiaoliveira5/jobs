b3b3f56c1917266211650ebc1d1d2efd044f90ad
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-22 13:39:53 -0300

    tests for component

--- spec/components/login_component_spec.rb
+++ spec/components/login_component_spec.rb
@@ -1,15 +1,19 @@
-# frozen_string_literal: true
-
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe LoginComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:rendered_component) { render_inline(described_class.new).to_html }
+  it 'renders the login component' do
+    expect(rendered_component).to have_css('.tabs')
+    expect(rendered_component).to have_css('.tabcontent')
+    expect(rendered_component).to have_css('.tab', text: 'Candidato')
+    expect(rendered_component).to have_css('.tab', text: 'Empresa')
+    expect(rendered_component).to have_link('Login')
+  end
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'had the input fields and labels' do
+    expect(rendered_component).to have_css('input', count: 4)
+    expect(rendered_component).to have_css('label', text: 'Senha', count: 2)
+    expect(rendered_component).to have_css('label', exact_text: 'E-mail', count: 1)
+    expect(rendered_component).to have_css('label', exact_text: 'E-mail corporativo', count: 1)
+  end
 end
