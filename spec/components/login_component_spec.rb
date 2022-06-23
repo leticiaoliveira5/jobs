9c0077d33ee51d7ec49984f0f0b7d73eb763e25f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-22 21:17:12 -0300

    updates component tests

--- spec/components/login_component_spec.rb
+++ spec/components/login_component_spec.rb
@@ -8,11 +8,10 @@ RSpec.describe LoginComponent, type: :component do
     expect(rendered_component).to have_css('.tabcontent')
     expect(rendered_component).to have_css('.tab', text: 'Candidato')
     expect(rendered_component).to have_css('.tab', text: 'Empresa')
-    expect(rendered_component).to have_link('Login')
+    expect(rendered_component).to have_button('Login')
   end
 
   it 'had the input fields and labels' do
-    expect(rendered_component).to have_css('input', count: 4)
     expect(rendered_component).to have_css('label', text: 'Senha', count: 2)
     expect(rendered_component).to have_css('label', exact_text: 'E-mail', count: 1)
     expect(rendered_component).to have_css('label', exact_text: 'E-mail corporativo', count: 1)
