db3ac0e7c3286c53bceb41f646670ebc0a1d052b
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-25 18:01:11 -0300

    cc test

--- spec/components/login_form_component_spec.rb
+++ spec/components/login_form_component_spec.rb
@@ -1,21 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe LoginFormComponent, type: :component do
-  it 'renders the candidate form' do
-    rendered_component =
-      render_inline(described_class.new(resource_name: 'candidate')).to_html
 
-    expect(rendered_component).to have_css('label', text: 'Senha', count: 1)
-    expect(rendered_component).to have_css('label', exact_text: 'E-mail', count: 1)
-    expect(rendered_component).to have_button('Login')
+  def rendered_component(resource_name)
+    render_inline(described_class.new(resource_name: resource_name)).to_html
+  end
+
+  it 'renders the candidate form' do
+    view = rendered_component('candidate')
+
+    expect(view).to have_css('label', text: 'Senha', count: 1)
+    expect(view).to have_css('label', exact_text: 'E-mail', count: 1)
+    expect(view).to have_button('Login')
   end
 
   it 'renders the employee form' do
-    rendered_component =
-      render_inline(described_class.new(resource_name: 'employee')).to_html
+    view = rendered_component('employee')
 
-    expect(rendered_component).to have_css('label', text: 'Senha', count: 1)
-    expect(rendered_component).to have_css('label', exact_text: 'E-mail corporativo', count: 1)
-    expect(rendered_component).to have_button('Login')
+    expect(view).to have_css('label', text: 'Senha', count: 1)
+    expect(view).to have_css('label', exact_text: 'E-mail corporativo', count: 1)
+    expect(view).to have_button('Login')
   end
 end
