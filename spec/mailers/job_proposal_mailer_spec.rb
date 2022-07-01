24c46fa8e2da5b8d2f1a44eacf5f2f8ce64736a4
leticiaoliveira5 <leoliveira994@gmail.com>
2022-07-01 11:16:41 -0300

    fix test issue

--- spec/mailers/job_proposal_mailer_spec.rb
+++ spec/mailers/job_proposal_mailer_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 describe JobProposalMailer do
   describe '#notify_proposal' do
-    let!(:company) { create(:company) }
-    let!(:candidate) { create(:candidate, firstname: 'Juliana', email: 'juliana@mail.com') }
-    let!(:job_opportunity) { create(:job_opportunity, job_title: 'Desenvolvedor', company: company) }
-    let!(:job_application) { create(:job_application, candidate: candidate, job_opportunity: job_opportunity) }
-    let(:job_proposal) { create(:job_proposal, job_application: job_application, candidate: candidate) }
+    let(:job_proposal) { create(:job_proposal) }
 
     it 'should build and email correctly' do
       mail = described_class.notify_proposal(job_proposal.id)
+      expected_text = "#{job_proposal.candidate.firstname}, sua candidatura à vaga"
 
       expect(mail.subject).to eq 'Você recebeu uma proposta!'
-      expect(mail.to).to include 'juliana@mail.com'
-      expect(mail.to).to eq ['juliana@mail.com']
-      expect(mail.body).to include 'Juliana, sua candidatura à vaga'
+      expect(mail.to).to include job_proposal.candidate.email
+      expect(mail.to).to eq [job_proposal.candidate.email]
+      expect(mail.body).to include expected_text
     end
   end
 end
