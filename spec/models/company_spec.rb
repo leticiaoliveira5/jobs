d1e472ccae393c0c7ef848e1e35a6d1e5cec7368
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-07-01 10:18:31 -0300

    refactors model tests (#87)
    * refactors some tests
    
    * fixes some issues
    
    * fixes issue
    
    * fixes issues
    
    * aligne elements
    
    * fixes some issues
    
    * fixes test
    
    * better test
    
    * fixes issue
    
    * Update job_opportunity_spec.rb
    
    * Update spec/models/job_opportunity_spec.rb
    
    * Update spec/models/job_opportunity_spec.rb
    
    * rubocop
    
    * changes place of test
    
    * fixes more issues
    
    * test passing
    
    * :)
    
    * Update job_opportunity_spec.rb

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Company, type: :model do
   context 'creation' do
-    it 'is created after first employee' do
-      first_employee = Employee.create!(firstname: 'Ronald',
-                                        surname: 'McDonald',
-                                        email: 'ronald@mcdonalds.com',
-                                        password: '123456')
-
-      expect(Company.count).to eq(1)
-      expect(Company.all.first.domain).to eq('mcdonalds.com')
-      expect(first_employee.admin?).to eq true
+    let!(:first_employee) do
+      Employee.create!(firstname: 'Ronald',
+                       surname: 'McDonald',
+                       email: 'ronald@mcdonalds.com',
+                       password: '123456')
     end
+
+    it { expect(described_class.count).to eq(1) }
+    it { expect(described_class.first.domain).to eq('mcdonalds.com') }
+    it { expect(first_employee.admin?).to eq true }
   end
 end
