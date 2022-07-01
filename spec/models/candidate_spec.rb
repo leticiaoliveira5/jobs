85ff163ec934c18438903fb791da0905ef0c7a10
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-30 21:43:39 -0300

    refactors some tests

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
-  context '#create_resume' do
-    it 'creates resume' do
-      candidate = Candidate.create(email: 'maria@gmail.com',
-                                   password: '123456',
-                                   firstname: 'Maria',
-                                   surname: 'Costa')
-
-      expect(Resume.count).to eq(1)
-      expect(candidate.resume).to be_truthy
+  context '#after_create' do
+    it 'creates resume after candidate creation' do
+      expect do
+        described_class.create(email: 'maria@gmail.com',
+                               password: '123456',
+                               firstname: 'Maria',
+                               surname: 'Costa')
+      end.to change(Resume, :count).by(1)
     end
   end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,59 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  describe 'validations' do
-    it 'is not valid without basic attributes' do
-      employee = Employee.new(company_id: nil)
+  describe '#before_validation' do
+    let(:subject) do
+      described_class.create!(firstname: 'Steve',
+                              surname: 'Jobs',
+                              email: 'steve@apple.com',
+                              password: '123456')
+    end
 
-      expect(employee).not_to be_valid
-      expect(employee.errors).to include(:email,
-                                         :password,
-                                         :firstname,
-                                         :surname,
-                                         :company)
+    it 'creates company' do
+      expect{subject}.to change(Company, :count).by(1)
+      expect(Company.first.domain).to eq('apple.com')
+    end
+
+    it 'first employee is admin' do
+      expect(subject.admin?).to eq true
+      expect(subject.role).to eq 'admin'
     end
   end
 
-  context 'company' do
-    it 'belongs to existing company if has the same domain' do
-      first_employee = Employee.create!(firstname: 'Ronald',
-                                        surname: 'McDonald',
-                                        email: 'ronald@mcdonalds.com',
-                                        password: '123456')
-      second_employee = Employee.create!(firstname: 'Bob',
-                                         surname: 'Spy',
-                                         email: 'bob@mcdonalds.com',
-                                         password: '123456')
+  describe 'validations' do
+    let(:subject) { described_class.new(company_id: nil) }
+  
+    it 'is not valid without basic attributes' do
+      expect(subject).not_to be_valid
+      expect(subject.errors).to include(:email,
+                                        :password,
+                                        :firstname,
+                                        :surname,
+                                        :company)
+    end
+  end
 
+  describe 'company' do
+    let!(:first_employee) do
+      described_class.create!(firstname: 'Ronald',
+                              surname: 'McDonald',
+                              email: 'ronald@mcdonalds.com',
+                              password: '123456')
+    end
+
+    let!(:second_employee) do
+      described_class.create!(firstname: 'Bob',
+                              surname: 'Spy',
+                              email: 'bob@mcdonalds.com',
+                              password: '123456')
+    end
+
+    it 'belongs to existing company if has the same domain' do
       expect(Company.count).to eq(1)
       expect(second_employee.company).to eq(first_employee.company)
     end
-  end
 
-  context 'role' do
-    it 'first employee is admin' do
-      first_employee = Employee.create!(firstname: 'Ronald',
-                                        surname: 'McDonald',
-                                        email: 'ronald@mcdonalds.com',
-                                        password: '123456')
-
-      expect(Company.count).to eq(1)
-      expect(Company.all.first.domain).to eq('mcdonalds.com')
-      expect(first_employee.admin?).to eq true
-      expect(first_employee.role).to eq 'admin'
-    end
-
-    it 'other employees are regulars' do
-      first_employee = Employee.create!(firstname: 'Ronald',
-                                        surname: 'McDonald',
-                                        email: 'ronald@mcdonalds.com',
-                                        password: '123456')
-      second_employee = Employee.create!(firstname: 'Bob',
-                                         surname: 'Spy',
-                                         email: 'bob@mcdonalds.com',
-                                         password: '123456')
-
-      expect(Company.count).to eq(1)
+    it 'only the first employee is admin' do
       expect(first_employee.admin?).to eq true
       expect(second_employee.admin?).to eq false
       expect(second_employee.regular?).to eq true
