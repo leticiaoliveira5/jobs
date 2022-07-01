5a97134359caa92d07985c934e6d8d37ec7f292f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-06-30 22:43:20 -0300

    changes place of test

--- spec/models/job_opportunity_spec.rb
+++ spec/models/job_opportunity_spec.rb
@@ -5,24 +5,8 @@ RSpec.describe JobOpportunity, type: :model do
     it 'limit date cannot be in the past' do
       job_opportunity = build(:job_opportunity, limit_date: '18/11/1958')
 
-      expect(job_opportunity.valid?).to eq false
+      expect(job_opportunity).not_to be_valid
       expect(job_opportunity.errors).to include(:limit_date)
-    end
-  end
-
-  describe '#check_number_of_positions' do
-    let!(:job_application) { create(:job_application) }
-    let(:job_proposal) do
-      create(:job_proposal, job_application: job_application,
-                            candidate: job_application.candidate,
-                            job_opportunity: job_application.job_opportunity)
-    end
-
-    it 'is deactivated when candidate accepts proposal' do
-      job_proposal.accepted!
-      job_opportunity = job_proposal.job_opportunity
-
-      expect { job_proposal.check_number_of_positions }.to change(job_opportunity, :status).to('inactive')
     end
   end
 end
--- spec/models/job_proposal_spec.rb
+++ spec/models/job_proposal_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe JobProposal, type: :model do
-  context 'validation' do
+  describe 'validation' do
     it 'can not be created without fields' do
       company = create(:company)
       job_opportunity = create(:job_opportunity, company: company)
@@ -16,6 +16,22 @@ RSpec.describe JobProposal, type: :model do
                                         job_opportunity: job_opportunity)
       expect(JobProposal.count).to eq(0)
       expect(job_proposal.errors.count).to eq(3)
+    end
+  end
+
+  describe '#check_number_of_positions' do
+    let!(:job_application) { create(:job_application) }
+    let(:job_proposal) do
+      create(:job_proposal, job_application: job_application,
+                            candidate: job_application.candidate,
+                            job_opportunity: job_application.job_opportunity)
+    end
+
+    it 'is deactivated when candidate accepts proposal' do
+      job_proposal.accepted!
+      job_opportunity = job_proposal.job_opportunity
+
+      expect { job_proposal.check_number_of_positions }.to change(job_opportunity, :status).to('inactive')
     end
   end
 end
