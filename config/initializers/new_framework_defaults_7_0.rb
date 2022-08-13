3ae0d1778e6e62c471e77de3111310c94cbcf775
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-12 21:25:09 -0300

    cookies_serializer

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -89,6 +89,11 @@ Rails.application.config.action_controller.raise_on_open_redirects = true
 # operations. See the upgrading guide for detail on the changes required.
 # The `:mini_magick` option is not deprecated; it's fine to keep using it.
 # Rails.application.config.active_storage.variant_processor = :vips
+# =====================================
+# =====================================
+# =======^^BREAKS^^^==================
+# =====================================
+# =====================================
 
 # If you're upgrading and haven't set `cookies_serializer` previously, your cookie serializer
 # was `:marshal`. Convert all cookies to JSON, using the `:hybrid` formatter.
@@ -100,7 +105,7 @@ Rails.application.config.action_controller.raise_on_open_redirects = true
 # If you have configured the serializer elsewhere, you can remove this.
 #
 # See https://guides.rubyonrails.org/action_controller_overview.html#cookies for more information.
-# Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :hybrid
 
 # Enable parameter wrapping for JSON.
 # Previously this was set in an initializer. It's fine to keep using that initializer if you've customized it.
