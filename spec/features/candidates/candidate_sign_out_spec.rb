92e25394d0c9a3f070f17b25a0dbfd9a18018162
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-13 21:41:01 -0300

    fix cc

--- spec/features/candidates/candidate_sign_out_spec.rb
+++ spec/features/candidates/candidate_sign_out_spec.rb
@@ -7,7 +7,7 @@ feature 'Candidate sign out' do
     login_as candidate, scope: :candidate
     visit candidates_path(candidate)
     within('.nav') { click_on('Sair') }
-    
+
     expect(current_path).to eq root_path
   end
 end
