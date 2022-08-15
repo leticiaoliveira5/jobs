cafae4add0c8155a4eff4f896349d13acdcb6e3b
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 20:59:49 -0300

    search results contrl covered

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,10 +1,6 @@
 class SearchResultsController < ApplicationController
   def index
     input = params[:search_input]
-    @job_opportunities_search = if input.blank?
-                                  nil
-                                else
-                                  JobOpportunity.search(input)
-                                end
+    @job_opportunities_search = JobOpportunity.search(input) if input.present?
   end
 end
