edd930a0373828c4eebcce09af56eea2e6fad8dc
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 19:55:26 -0300

    params to a variable

--- spec/controllers/job_proposals_controller_spec.rb
+++ spec/controllers/job_proposals_controller_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe JobProposalsController, type: :controller, login_metadata: true d
   let(:candidate) { job_proposal.candidate }
   let(:job_application) { job_proposal.job_application }
   let(:employee) { create(:employee, company: job_application.company) }
+  let(:job_proposal_params) { { id: job_proposal.id, job_application_id: job_application.id } }
 
   describe '#show', candidate_signed_in: true do
     it 'renders show' do
-      get :show, params: { id: job_proposal.id, job_application_id: job_application.id }
+      get :show, params: job_proposal_params
 
       expect(response).to render_template('show')
     end
@@ -16,7 +17,7 @@ RSpec.describe JobProposalsController, type: :controller, login_metadata: true d
 
   describe '#new', employee_signed_in: true do
     it 'renders new' do
-      get :new, params: { id: job_proposal.id, job_application_id: job_application.id }
+      get :new, params: job_proposal_params
 
       expect(response).to render_template('new')
     end
@@ -52,7 +53,7 @@ RSpec.describe JobProposalsController, type: :controller, login_metadata: true d
 
   describe '#accept', candidate_signed_in: true do
     it 'updates status and redirects with flash message' do
-      post :accept, params: { id: job_proposal.id, job_application_id: job_application.id }
+      post :accept, params: job_proposal_params
 
       expect(response).to redirect_to job_application_job_proposal_path(job_proposal)
       expect(job_proposal.reload.status).to eq('accepted')
@@ -62,7 +63,7 @@ RSpec.describe JobProposalsController, type: :controller, login_metadata: true d
 
   describe '#reject', candidate_signed_in: true do
     it 'updates status and redirects with flash message' do
-      post :reject, params: { id: job_proposal.id, job_application_id: job_application.id }
+      post :reject, params: job_proposal_params
 
       expect(response).to redirect_to job_application_job_proposal_path(job_proposal)
       expect(job_proposal.reload.status).to eq('rejected')
