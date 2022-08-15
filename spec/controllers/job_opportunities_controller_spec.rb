cdf82e42d04a6faf5c705e916eb4089ba4f5505a
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-14 23:14:22 -0300

    more tests

--- spec/controllers/job_opportunities_controller_spec.rb
+++ spec/controllers/job_opportunities_controller_spec.rb
@@ -4,14 +4,23 @@ RSpec.describe JobOpportunitiesController, type: :controller do
   let(:employee) { create(:employee) }
   let(:job_opportunity) { create(:job_opportunity, company: employee.company) }
 
-  describe '#show' do
-    it_behaves_like 'controller simple get action',
-                    object: 'job_opportunity', action: 'show', template: 'show'
-  end
-
   describe '#index' do
     it_behaves_like 'controller simple get action',
                     object: 'job_opportunity', action: 'index', template: 'index'
+  end
+
+  describe '#new' do
+    before do
+      sign_in(employee)
+      get :new
+    end
+
+    it { expect(response).to render_template('new') }
+  end
+
+  describe '#show' do
+    it_behaves_like 'controller simple get action',
+                    object: 'job_opportunity', action: 'show', template: 'show'
   end
 
   describe '#edit' do
@@ -24,12 +33,29 @@ RSpec.describe JobOpportunitiesController, type: :controller do
     it { expect(assigns(:job_opportunity)).to eq job_opportunity }
   end
 
-  describe '#new' do
-    before do
+  describe '#create' do
+    it 'with valid params, creates job_opportunity' do
       sign_in(employee)
-      get :new
+
+      expect do
+        post :create, params: { job_opportunity: { job_title: 'Cantor',
+                                                   description: 'Descrição teste',
+                                                   job_level: '1',
+                                                   salary_range: '200',
+                                                   place: 'Location',
+                                                   limit_date: 6.months.from_now,
+                                                   number_of_positions: 2 } }
+      end.to change(JobOpportunity, :count).by(1)
+
+      expect(response).to redirect_to(job_opportunity_path(JobOpportunity.last))
     end
 
-    it { expect(response).to render_template('new') }
+    it 'with invalis params, renders new' do
+      sign_in(employee)
+
+      post :create, params: { job_opportunity: { job_title: 'Cantor' } }
+
+      expect(response).to render_template('new')
+    end
   end
 end
