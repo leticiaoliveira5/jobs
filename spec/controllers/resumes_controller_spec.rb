6e52599385ad7eccf5c9482f24b02cd2e110a7bb
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 17:59:06 -0300

    refactors resumes contrl test

--- spec/controllers/resumes_controller_spec.rb
+++ spec/controllers/resumes_controller_spec.rb
@@ -4,21 +4,22 @@ RSpec.describe ResumesController, type: :controller do
   let(:candidate) { create(:candidate) }
   let(:resume) { candidate.resume }
 
+  before do |test|
+    sign_in(candidate) unless test.metadata[:logged_out]
+  end
+
   describe '#show' do
     context 'when candidate is signed in' do
-      before do
-        sign_in(candidate)
-        get :show, params: { id: resume.id }
-      end
-
       it 'renders template show' do
+        get :show, params: { id: resume.id }
+
         expect(response).to render_template('show')
         expect(assigns(:resume)).to eq resume
       end
     end
 
     context 'when candidate or employee is not signed in' do
-      it 'redirects to home page' do
+      it 'redirects to home page', logged_out: true do
         get :show, params: { id: resume.id }
 
         expect(response).to redirect_to root_path
@@ -28,24 +29,18 @@ RSpec.describe ResumesController, type: :controller do
   end
 
   describe '#edit' do
-    before do
-      sign_in(candidate)
-      get :edit, params: { id: resume.id }
-    end
-
     it 'renders template edit' do
+      get :edit, params: { id: resume.id }
+
       expect(response).to render_template('edit')
       expect(assigns(:resume)).to eq resume
     end
   end
 
   describe '#update' do
-    before do
-      sign_in(candidate)
-      patch :update, params: { id: resume.id, resume: { address: 'Paraíba' } }
-    end
-
     it 'updates resume and redirects to show' do
+      patch :update, params: { id: resume.id, resume: { address: 'Paraíba' } }
+
       expect(resume.reload.address).to eq 'Paraíba'
       expect(response).to redirect_to resume_path(resume)
       expect(flash[:notice]).to be_present
