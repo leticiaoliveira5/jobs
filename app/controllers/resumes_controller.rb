bd660291f3cf31b0a077ac7aace94bcae59deed0
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 14:08:57 -0300

    removes unused routes

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,17 +1,11 @@
 class ResumesController < ApplicationController
   before_action :authenticate_candidate!, only: %i[create edit update]
 
-  def index; end
-
   def show
     unless candidate_signed_in? || employee_signed_in?
       redirect_to root_path, alert: 'Você não tem permissão para ver esta página'
     end
     @resume = Resume.joins(:candidate).find(params[:id])
-  end
-
-  def create
-    @resume = Resume.new(resume_params)
   end
 
   def edit
