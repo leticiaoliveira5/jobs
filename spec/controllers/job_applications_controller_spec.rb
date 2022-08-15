4bd88ac4d53bfc052e2685d13f26e61723dbd1ca
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 17:45:28 -0300

    refactoring job application controller test

--- spec/controllers/job_applications_controller_spec.rb
+++ spec/controllers/job_applications_controller_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe JobApplicationsController, type: :controller do
+  let(:candidate) { create(:candidate) }
+  let(:job_application) { create(:job_application, candidate: candidate) }
+  let(:employee) { create(:employee, company: job_application.company) }
+
   describe '#show' do
     it_behaves_like 'controller simple get action',
                     object: 'job_application', action: 'show', template: 'show'
   end
 
   describe '#destroy' do
-    it 'deletes job application' do
-      candidate = create(:candidate)
+    it 'deletes job application and redirects' do
       job_application = create(:job_application, candidate: candidate)
 
       sign_in(candidate)
@@ -18,20 +21,18 @@ RSpec.describe JobApplicationsController, type: :controller do
       end.to change(JobApplication, :count).by(-1)
 
       expect(response).to redirect_to(candidates_path(candidate))
+      expect(flash[:notice]).to be_present
     end
   end
 
   describe '#decline' do
-    let!(:job_application) { create(:job_application) }
-    let(:employee) { create(:employee, company: job_application.company) }
-
-    before do
-      sign_in(employee)
-      post :decline, params: { id: job_application.id }
-    end
-
     it 'updates status and sends flash message' do
+      sign_in(employee)
+
+      post :decline, params: { id: job_application.id }
+
       expect(job_application.reload.status).to eq 'declined'
+      expect(response).to redirect_to company_path(job_application.company)
       expect(flash[:notice]).to be_present
     end
   end
