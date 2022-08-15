daa7704797c4d642582ae704bef5787fa6fb70ba
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-15 11:46:05 -0300

    better shared example

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,7 +1,8 @@
 shared_examples 'controller simple get action' do |object: '', action: '', template: ''|
   let(:instance) { create(object.to_sym) }
+
   before { get action.to_sym, params: { id: instance.id } }
 
   it { expect(response).to render_template(template) }
-  it { expect(assigns(object.to_sym)).to eq instance } if action != 'index'
+  it { expect(assigns(object.to_sym)).to eq instance } unless action.in?(['index', 'new'])
 end
