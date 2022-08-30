7ace621e4df17a027196abd1ffff1227c136674b
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-30 10:13:39 -0300

    uses component on views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     elsif employee_signed_in?
       redirect_to company_path(current_employee.company)
     else
-      @job_opportunities = JobOpportunity.all
+      @job_opportunities = JobOpportunity.first(6)
       @current_user = nil
     end
   end
