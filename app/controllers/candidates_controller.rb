44ec11c12f9d2200384d1ac3cfa662084d9eecc8
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 15:54:37 -0300

    adds basic icon

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,11 +1,15 @@
 class CandidatesController < ApplicationController
+  include ActionView::Helpers::AssetTagHelper
   before_action :authenticate_candidate!
 
   def index
+    return unless candidate_signed_in?
+
+    icon = current_candidate.icon
     @avatar = if icon.attached?
                 image_tag(icon, size: '50x50', style: 'border-radius: 50%;')
               else
-                image_tag(size: '50x50')
+                image_tag('icon.png', size: '50x50')
               end
   end
 end
