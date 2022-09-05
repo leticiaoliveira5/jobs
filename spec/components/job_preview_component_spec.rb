8bdb2792c5bc7cae3f28925c723dfb0730a432df
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 14:25:51 -0300

    rubocop

--- spec/components/job_preview_component_spec.rb
+++ spec/components/job_preview_component_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe JobPreviewComponent, type: :component do
   let(:rendered_component) do
     render_inline(described_class.new(job_opportunity: job_opportunity)).to_html
   end
+  let(:path) { "/job_opportunities/#{job_opportunity.id}" }
 
   it 'renders the component' do
     expect(rendered_component).to have_css('.job-preview-box')
-    expect(rendered_component).to have_link('Ver detalhes', href: "/job_opportunities/#{job_opportunity.id}")
+    expect(rendered_component).to have_link('Ver detalhes', href: path)
     expect(rendered_component).to have_css('h4', text: job_opportunity.job_title)
     expect(rendered_component).to have_css('p', text: job_opportunity.job_level)
     expect(rendered_component).to have_css('p', text: job_opportunity.place)
