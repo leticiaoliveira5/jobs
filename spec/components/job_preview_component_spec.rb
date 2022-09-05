65d99604aee81addb711e1038dc33198159a0ace
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 13:52:59 -0300

    component test

--- spec/components/job_preview_component_spec.rb
+++ spec/components/job_preview_component_spec.rb
@@ -3,13 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe JobPreviewComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:job_opportunity) { create(:job_opportunity) }
+  let(:rendered_component) { render_inline(described_class.new(job_opportunity: job_opportunity)).to_html }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders the component' do
+    expect(rendered_component).to have_css('.job-preview-box')
+    expect(rendered_component).to have_link('Ver detalhes')
+    expect(rendered_component).to have_css('h4', text: job_opportunity.job_title)
+    expect(rendered_component).to have_css('p', text: job_opportunity.job_level)
+    expect(rendered_component).to have_css('p', text: job_opportunity.place)
+  end
 end
