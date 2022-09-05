a3126610886c9f75d2354a5ed4f28e60873c4b01
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 13:59:45 -0300

    rubocop

--- spec/components/job_preview_component_spec.rb
+++ spec/components/job_preview_component_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 RSpec.describe JobPreviewComponent, type: :component do
   let(:job_opportunity) { create(:job_opportunity) }
-  let(:rendered_component) { render_inline(described_class.new(job_opportunity: job_opportunity)).to_html }
+  let(:rendered_component) do
+    render_inline(described_class.new(job_opportunity: job_opportunity)).to_html
+  end
 
   it 'renders the component' do
     expect(rendered_component).to have_css('.job-preview-box')
