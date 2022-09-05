817fbd8091c323ce94458eddc29ce1ae316a9b4c
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 13:19:42 -0300

    grid component options

--- app/components/grid_component.rb
+++ app/components/grid_component.rb
@@ -1,5 +1,21 @@
-# frozen_string_literal: true
-
 class GridComponent < ViewComponent::Base
+  def initialize(params)
+    super
+    @column_gap = params[:column_gap]
+    @row_gap = params[:row_gap]
+    @columns = params[:columns]
+    @rows = params[:rows]
+  end
 
+  def columns_css
+    return unless @columns
+
+    'auto ' * @columns.to_i
+  end
+
+  def rows_css
+    return unless @row
+
+    'auto ' * @rows.to_i
+  end
 end
