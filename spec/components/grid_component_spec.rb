e16f0b1c8475080877c5286ea99bf81de9be19da
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 14:16:09 -0300

    increase coverage

--- spec/components/grid_component_spec.rb
+++ spec/components/grid_component_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe GridComponent, type: :component do
-  let(:rendered_component) { render_inline(described_class.new).to_html }
+  let(:rendered_component) { render_inline(described_class.new(params)).to_html }
+  let(:params) { { columns: 2, rows: 2 } }
 
   it 'renders the component' do
     expect(rendered_component).to have_css('.grid')
