d7ae3ae3d67d479540b00b36c3fe4d882559026d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 15:41:23 -0300

    displays icon (test)

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,11 @@
 class CandidatesController < ApplicationController
   before_action :authenticate_candidate!
 
-  def index; end
+  def index
+    @avatar = if icon.attached?
+                image_tag(icon, size: '50x50', style: 'border-radius: 50%;')
+              else
+                image_tag(size: '50x50')
+              end
+  end
 end
