bba132683e0cdaad43bb33a96f7d4bcf99df8c3b
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-06 11:23:35 -0300

    fix cc

--- spec/features/employee/employee_changes_job_opportunity_status_spec.rb
+++ spec/features/employee/employee_changes_job_opportunity_status_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
-feature 'Employee edits job opportunity' do
+feature 'Employee changes job opportunity status' do
   let!(:employee) { create(:employee) }
   let!(:job_opportunity) do
-    create(:job_opportunity, job_title: 'Dummie',
-                             description: 'Auxiliar de eventos',
-                             company: employee.company,
-                             status: 'active')
+    create(:job_opportunity, job_title: 'Dummie', description: 'Auxiliar de eventos',
+                             company: employee.company, status: 'active')
   end
 
-  before do
+  scenario 'inactivation' do
     login_as employee, scope: :employee
-    visit root_path
-    click_on 'Área da empresa'
-    click_on 'Dummie'
-  end
-
-  scenario 'inactivates and activates' do
+    visit job_opportunity_path(job_opportunity)
     click_on 'Inativar vaga'
 
     expect(current_path).to eq job_opportunity_path(job_opportunity)
     expect(job_opportunity.reload.status).to eq 'inactive'
     expect(page).to have_text('Inativa')
     expect(page).to have_link('Ativar vaga')
+  end
 
+  scenario 'activation' do
+    job_opportunity.inactive!
+
+    login_as employee, scope: :employee
+    visit job_opportunity_path(job_opportunity)
     click_on 'Ativar vaga'
 
     expect(current_path).to eq job_opportunity_path(job_opportunity)
