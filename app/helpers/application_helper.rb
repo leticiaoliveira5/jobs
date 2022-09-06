026ceacddec905c6040e533e2f7731a2256e255e
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-05 21:44:46 -0300

    rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,19 @@
 module ApplicationHelper
-
   # crete component for these methods
   def inactivate_link(job_opportunity)
     return unless job_opportunity.inactive?
 
-    link_to 'Inativar vaga', inactivate_job_opportunity_job_opportunity_path(job_opportunity), method: :post
+    link_to 'Inativar vaga',
+            inactivate_job_opportunity_job_opportunity_path(job_opportunity),
+            method: :post
   end
 
   def activate_link(job_opportunity)
     return unless job_opportunity.active?
 
-    link_to 'Ativar vaga', activate_job_opportunity_job_opportunity_path(job_opportunity), method: :post
+    link_to 'Ativar vaga',
+            activate_job_opportunity_job_opportunity_path(job_opportunity),
+            method: :post
   end
 
   def edit_link(job_opportunity)
@@ -18,14 +21,16 @@ module ApplicationHelper
   end
 
   def apply_link(job_opportunity, candidate)
-    return unless candidate.present?
+    return if candidate.blank?
 
     candidate_applied = job_opportunity.candidates.include?(candidate)
 
     if candidate_applied
       'Você está inscrito nesta vaga.'
     elsif !candidate_applied && job_opportunity.active?
-      link_to 'Inscrever-se nesta vaga', create_job_application_job_opportunity_path(@job_opportunity), method: :post
+      link_to 'Inscrever-se nesta vaga',
+              create_job_application_job_opportunity_path(job_opportunity),
+              method: :post
     end
   end
 end
