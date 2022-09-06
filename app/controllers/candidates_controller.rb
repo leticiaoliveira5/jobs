ff0aef940de0ab4afe1ad6dc13a10d1cd2f553fc
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-06 09:57:19 -0300

    removes changes from candidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,15 +1,5 @@
 class CandidatesController < ApplicationController
-  include ActionView::Helpers::AssetTagHelper
   before_action :authenticate_candidate!
 
-  def index
-    return unless candidate_signed_in?
-
-    avatar = current_candidate.avatar
-    @avatar = if avatar.attached?
-                image_tag(avatar, size: '50x50', style: 'border-radius: 50%;')
-              else
-                image_tag('icon.png', size: '50x50')
-              end
-  end
+  def index; end
 end
