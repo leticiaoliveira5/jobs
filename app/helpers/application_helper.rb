ac87755d2fc6633969218d33cb66c5dd7700e19b
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-06 09:52:17 -0300

    helper method to display avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar.attached?
+      image_tag(user.avatar, size: '50x50', style: 'border-radius: 50%;')
+    else
+      image_tag('icon.png', size: '50x50')
+    end
+  end
 end
