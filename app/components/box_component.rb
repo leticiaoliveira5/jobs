3a148f94d86e81bed13a9d17eb76dae615b5ed10
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-06 17:38:05 -0300

    makes component collapsible

--- app/components/box_component.rb
+++ app/components/box_component.rb
@@ -3,4 +3,10 @@
 class BoxComponent < ViewComponent::Base
   renders_one :title
   renders_one :body
+
+  def initialize(collapsible: false, default: nil)
+    @collapsible = collapsible
+    @default = default
+    super
+  end
 end
