3ecc6be87a1e9fcfde2596d74c4b639d6d307d38
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-09-06 10:06:51 -0300

    User icon (#105)
    * creates active storage tables
    
    * active_storage setup
    
    * permit param
    
    * candidate and employee have icons
    
    * file input
    
    * displays icon (test)
    
    * adds basic icon
    
    * avatar => icon
    
    * helper method to display avatar
    
    * refactors helper method
    
    * removes changes from candidates controller
    
    * ignore migrate files (codeclimate)
    
    * codeclimate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_avatar(user)
+    avatar = user.avatar.attached? ? user.avatar : 'icon.png'
+    image_tag(avatar, size: '50x50', style: 'border-radius: 50%;')
+  end
 end
