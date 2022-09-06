3a6caeffafe6b2249b0247b1a52df6d282902ac1
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-06 10:27:48 -0300

    removes link logic from view completely

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
+  include Devise::Controllers::Helpers
+
   def user_avatar(user)
     avatar = user.avatar.attached? ? user.avatar : 'icon.png'
     image_tag(avatar, size: '50x50', style: 'border-radius: 50%;')
   end
-  
-    # crete component for these methods
+
+  # crete component for these methods
   def inactivate_link(job_opportunity)
-    return unless job_opportunity.inactive?
+    return unless employee_signed_in? && current_employee.company == job_opportunity.company
+    return if job_opportunity.inactive?
 
     link_to 'Inativar vaga',
             inactivate_job_opportunity_job_opportunity_path(job_opportunity),
@@ -14,7 +17,8 @@ module ApplicationHelper
   end
 
   def activate_link(job_opportunity)
-    return unless job_opportunity.active?
+    return unless employee_signed_in? && current_employee.company == job_opportunity.company
+    return if job_opportunity.active?
 
     link_to 'Ativar vaga',
             activate_job_opportunity_job_opportunity_path(job_opportunity),
@@ -22,18 +26,28 @@ module ApplicationHelper
   end
 
   def edit_link(job_opportunity)
+    return unless employee_signed_in? && current_employee.company == job_opportunity.company
+
     link_to 'Editar vaga', edit_job_opportunity_path(job_opportunity)
   end
 
-  def apply_link(job_opportunity, candidate)
-    candidate_applied = job_opportunity.candidates.include?(candidate) if candidate.present?
+  def apply_link(job_opportunity)
+    return unless candidate_signed_in?
 
-    if candidate_applied
+    if job_opportunity.candidates.include?(current_candidate)
       'Você está inscrito nesta vaga.'
-    elsif !candidate_applied && job_opportunity.active?
+    elsif candidate.present? && !candidate_applied && job_opportunity.active?
       link_to 'Inscrever-se nesta vaga',
               create_job_application_job_opportunity_path(job_opportunity),
               method: :post
     end
+  end
+
+  def sign_up_and_apply_link(job_opportunity)
+    return if employee_signed_in? || candidate_signed_in?
+
+    link_to 'Faça login para inscrever-se nesta vaga',
+            create_job_application_job_opportunity_path(job_opportunity),
+            method: :post
   end
 end
