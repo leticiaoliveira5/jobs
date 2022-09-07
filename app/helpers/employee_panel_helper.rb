bda6946fe380d0b7a066ca0851c7661cbbd241b6
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-07 18:00:36 -0300

    rubocop

--- app/helpers/employee_panel_helper.rb
+++ app/helpers/employee_panel_helper.rb
@@ -1,7 +1,7 @@
 module EmployeePanelHelper
   def job_proposal_link(job_proposal)
-    link_name = "#{job_proposal.job_opportunity.job_title} - #{job_proposal.candidate.full_name} [#{job_proposal.status}]"
-    link_to link_name, job_application_job_proposal_path(job_proposal.job_application, job_proposal)
+    link_to "#{job_proposal.job_opportunity.job_title} - #{job_proposal.candidate.full_name}",
+            job_application_job_proposal_path(job_proposal.job_application, job_proposal)
   end
 
   def job_application_link(job_application)
