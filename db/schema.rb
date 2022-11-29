b4f1749dd4628cd0de1247c3e5237b0c1862540a
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-11-29 19:05:02 -0300

    Add document to candidate (#116)
    * add column document to candidate
    
    * replace cpf with document
    
    * fix validation
    
    * use factory in test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_201133) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_214321) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_201133) do
     t.string "about_me"
     t.integer "cpf"
     t.string "address"
+    t.string "document"
+    t.index ["document"], name: "index_candidates_on_document", unique: true
     t.index ["email"], name: "index_candidates_on_email", unique: true
     t.index ["reset_password_token"], name: "index_candidates_on_reset_password_token", unique: true
   end
