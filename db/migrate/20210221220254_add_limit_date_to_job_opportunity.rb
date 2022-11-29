ce54f3c8b4009ba4fc2f0ea30e3c253563528605
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-29 18:37:12 -0300

    update migration

--- db/migrate/20210221220254_add_limit_date_to_job_opportunity.rb
+++ db/migrate/20210221220254_add_limit_date_to_job_opportunity.rb
@@ -1,5 +1,5 @@
 class AddLimitDateToJobOpportunity < ActiveRecord::Migration[5.2]
   def change
-    add_column :job_opportunities, :limit_date, :date_field
+    add_column :job_opportunities, :limit_date, :datetime
   end
 end
