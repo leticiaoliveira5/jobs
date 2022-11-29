cca05c463d88516f27f124fe1e6e5d84dbd08e7f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-29 17:56:56 -0300

    fix rubocop warn 4 good

--- spec/features/employee/employee_views_job_applications_spec.rb
+++ spec/features/employee/employee_views_job_applications_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 feature 'Employee views job applications' do
-  let!(:apple) { create(:company, name: 'Apple', domain: 'apple.com') }
-  let(:apple_employee) { create(:employee, company: apple, email: 'employee@apple.com') }
-  let!(:microsoft) { create(:company, name: 'Microsoft', domain: 'microsoft.com') }
-  let(:microsoft_employee) { create(:employee, company: microsoft) }
+  let!(:apple) { create(:company, :with_employee, name: 'Apple', domain: 'apple.com') }
+  let!(:microsoft) { create(:company, :with_employee, name: 'Microsoft', domain: 'microsoft.com') }
   let(:job_opportunity) { create(:job_opportunity, company: apple, job_title: 'Desenvolvedor') }
   let!(:job_application) { create(:job_application, job_opportunity: job_opportunity) }
 
   scenario 'successfully' do
-    login_as apple_employee, scope: :employee
+    login_as apple.employees.first, scope: :employee
     visit job_opportunity_path(job_opportunity)
 
     expect(page).to have_content 'Candidaturas recebidas para esta vaga:'
@@ -17,7 +15,7 @@ feature 'Employee views job applications' do
   end
 
   scenario 'only if belongs to company' do
-    login_as microsoft_employee, scope: :employee
+    login_as microsoft.employees.first, scope: :employee
     visit root_path
     click_on 'Ver empresas cadastradas'
     click_on 'Apple'
