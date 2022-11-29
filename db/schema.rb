4d5df8e940bb757d5cba5088e89fe7e998e68068
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-11-29 18:02:12 -0300

    Add document to company (#114)
    * Create 20221129201133_add_document_to_company.rb
    
    * add unique index
    
    * run migration
    
    * replace cnpj with document
    
    * remove unwanted changes to schema
    
    * split tests (fix rubocop warn)
    
    * tests (fix rubocop warn)
    
    * fix rubocop warn 4 good
    
    * show document on page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_181028) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_201133) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -63,7 +63,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_181028) do
     t.string "name"
     t.string "address"
     t.integer "cnpj"
+    t.string "document"
     t.index ["cnpj"], name: "index_companies_on_cnpj", unique: true
+    t.index ["document"], name: "index_companies_on_document", unique: true
     t.index ["domain"], name: "index_companies_on_domain", unique: true
   end
 
--- spec/features/employee/employee_views_job_applications_spec.rb
+++ spec/features/employee/employee_views_job_applications_spec.rb
@@ -1,33 +1,21 @@
 require 'rails_helper'
 
 feature 'Employee views job applications' do
-  let(:candidate) { create(:candidate, firstname: 'Fernanda', surname: 'Braga') }
-  let!(:apple) { create(:company, name: 'Apple', domain: 'apple.com', cnpj: '12345678911234') }
-  let(:apple_employee) { create(:employee, company: apple, email: 'employee@apple.com') }
-  let!(:microsoft) do
-    create(:company, name: 'Microsoft', domain: 'microsoft.com', cnpj: '12345678911235')
-  end
-  let(:microsoft_employee) do
-    create(:employee, company: microsoft, email: 'employee01@microsoft.com')
-  end
-  let(:job_opportunity) do
-    create(:job_opportunity, company: apple, job_title: 'Desenvolvedor')
-  end
-
-  before do
-    create(:job_application, candidate: candidate, job_opportunity: job_opportunity)
-  end
+  let!(:apple) { create(:company, :with_employee, name: 'Apple', domain: 'apple.com') }
+  let!(:microsoft) { create(:company, :with_employee, name: 'Microsoft', domain: 'microsoft.com') }
+  let(:job_opportunity) { create(:job_opportunity, company: apple, job_title: 'Desenvolvedor') }
+  let!(:job_application) { create(:job_application, job_opportunity: job_opportunity) }
 
   scenario 'successfully' do
-    login_as apple_employee, scope: :employee
+    login_as apple.employees.first, scope: :employee
     visit job_opportunity_path(job_opportunity)
 
     expect(page).to have_content 'Candidaturas recebidas para esta vaga:'
-    expect(page).to have_content 'Desenvolvedor - Fernanda Braga'
+    expect(page).to have_content "Desenvolvedor - #{job_application.candidate.full_name}"
   end
 
   scenario 'only if belongs to company' do
-    login_as microsoft_employee, scope: :employee
+    login_as microsoft.employees.first, scope: :employee
     visit root_path
     click_on 'Ver empresas cadastradas'
     click_on 'Apple'
