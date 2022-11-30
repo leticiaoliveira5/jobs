2d2d694cd49ff0ee085df578ceaad81f1eb9e432
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 19:58:49 -0300

    add unit test

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,3 +1,5 @@
 class WorkExperience < ApplicationRecord
   belongs_to :candidate
+
+  validates :job_title, :start_date, presence: true
 end
--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe WorkExperience, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:candidate) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:job_title) }
+    it { should validate_presence_of(:start_date) }
+  end
 end
