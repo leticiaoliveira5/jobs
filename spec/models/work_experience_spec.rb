845eaa7cba5b22effdbe8818f2fb18933777f199
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 20:09:54 -0300

    fix rubocop

--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe WorkExperience, type: :model do
   describe 'associations' do
-    it { should belong_to(:candidate) }
+    it { is_expected.to belong_to(:candidate) }
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:job_title) }
-    it { should validate_presence_of(:start_date) }
+    it { is_expected.to validate_presence_of(:job_title) }
+    it { is_expected.to validate_presence_of(:start_date) }
   end
 end
