29d48ebe9c6d2616e328ca2d8290fde7cb48430c
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 17:16:48 -0300

    go back to resumes controller

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,37 @@
 class ResumesController < ApplicationController
+  before_action :authenticate_candidate!, :set_resume, except: :show
+
   def show
     unless candidate_signed_in? || employee_signed_in?
       redirect_to root_path, alert: 'Você não tem permissão para ver esta página'
     end
-    @resume = Resume.find(params[:id])
+    @resume = current_candidate&.resume || Resume.find(params[:id])
+    render template: 'resumes/show'
+  end
+
+  def edit
+    render template: 'resumes/edit'
+  end
+
+  def update
+    @resume.update(resume_params)
+    redirect_to resume_path, notice: t('.success') if @resume.save
+  end
+
+  private
+
+  # rubocop:disable Naming/MemoizedInstanceVariableName
+  def set_resume
+    @resume ||= current_candidate.resume
+  end
+  # rubocop:enable Naming/MemoizedInstanceVariableName
+
+  def resume_params
+    params.require(:resume).permit(:address,
+                                   :education,
+                                   :experience,
+                                   :languages,
+                                   :abilities,
+                                   :courses)
   end
 end
