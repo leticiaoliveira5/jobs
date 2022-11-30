e60ac84253a66f0d69e03b3b7eff9121c65dd1a8
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 15:25:02 -0300

    fix cc issue

--- app/controllers/candidate_resumes_controller.rb
+++ app/controllers/candidate_resumes_controller.rb
@@ -1,26 +1,23 @@
 class CandidateResumesController < ApplicationController
-  before_action :authenticate_candidate!
+  before_action :authenticate_candidate!, :set_resume
 
   def show
-    @resume = resume
     render template: 'resumes/show'
   end
 
   def edit
-    @resume = resume
     render template: 'resumes/edit'
   end
 
   def update
-    @resume = resume
     @resume.update(resume_params)
     redirect_to resume_path, notice: t('.success') if @resume.save
   end
 
   private
 
-  def resume
-    current_candidate.resume
+  def set_resume
+    @resume ||= current_candidate.resume
   end
 
   def resume_params
