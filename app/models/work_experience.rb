80d506b2a990a2ba5937a673dc0f12fab2282b17
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 20:53:24 -0300

    add validations and callback

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,5 +1,20 @@
 class WorkExperience < ApplicationRecord
+  before_save :set_status
+
   belongs_to :candidate
 
   validates :job_title, :start_date, presence: true
+  validate :end_date_is_after_start_date
+
+  enum status: { finished: 0, current: 1 }
+
+  def set_status
+    self.status = 0 if end_date < DateTime.now
+  end
+
+  def end_date_is_after_start_date
+    return if end_date.blank?
+
+    errors.add(:end_date, :invalid) if start_date > end_date
+  end
 end
--- spec/models/work_experience_spec.rb
+++ spec/models/work_experience_spec.rb
@@ -8,5 +8,20 @@ RSpec.describe WorkExperience, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:job_title) }
     it { is_expected.to validate_presence_of(:start_date) }
+
+    it 'validates if end_date is after start_date' do
+      obj = build(:work_experience, end_date: 2.weeks.ago, start_date: 1.day.ago)
+      expect(obj).not_to be_valid
+    end
+  end
+
+  describe '#set_status' do
+    it 'set status to finished if end date is before today' do
+      work_experience = build(:work_experience, start_date: 1.year.ago,
+                                                end_date: 3.months.ago,
+                                                status: nil)
+      work_experience.save
+      expect(work_experience.status).to eq 'finished'
+    end
   end
 end
