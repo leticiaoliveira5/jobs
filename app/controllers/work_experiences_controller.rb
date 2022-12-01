e38fcd0de2a47475e79d25e09d1ae87263b88392
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-01 18:08:18 -0300

    fix typo

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -2,18 +2,18 @@ class WorkExperiencesController < ApplicationController
   before_action :authenticate_candidate!
 
   def create
-    @work_experience = current_user&.work_experiences&.new
-    @work_experience.save
+    @work_experience = current_candidate&.work_experiences&.new
+    redirect_to candidate_path(current_candidate) if @work_experience.save
   end
 
   def update
-    @work_experience = current_user&.work_experiences&.find(params[:id])
+    @work_experience = current_candidate&.work_experiences&.find(params[:id])
     @work_experience.update(work_experience_params)
     @work_experience.save
   end
 
   def destroy
-    @work_experience = current_user&.work_experiences&.find(params[:id])
+    @work_experience = current_candidate&.work_experiences&.find(params[:id])
     @work_experience.destroy
   end
 
