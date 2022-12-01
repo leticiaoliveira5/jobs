f1a65bb18bf6b19d78873753fea5732e013899be
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-01 17:05:55 -0300

    fix test

--- spec/components/box_component_spec.rb
+++ spec/components/box_component_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 
 RSpec.describe BoxComponent, type: :component do
   let(:rendered_component) do
-    render_inline(described_class.new) do |component|
-      component.title { 'Test' }
+    render_inline(described_class.new()) do |component|
+      component.title { 'Title' }
       component.body { 'Body' }
     end.to_html
   end
 
   it 'renders the component' do
-    expect(rendered_component).to have_css('h2', text: 'Test')
+    expect(rendered_component).to have_css('.title', text: 'Title')
     expect(rendered_component).to have_css('.content', text: 'Body')
   end
 end
