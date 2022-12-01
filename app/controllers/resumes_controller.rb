c645d3d3729af162b8102b4a9608752b98540662
leticiaoliveira5 <leoliveira994@gmail.com>
2022-11-30 21:15:48 -0300

    undo changes

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,34 +1,8 @@
 class ResumesController < ApplicationController
-  before_action :authenticate_candidate!, :set_resume, except: :show
-
   def show
     unless candidate_signed_in? || employee_signed_in?
       redirect_to root_path, alert: 'Você não tem permissão para ver esta página'
     end
-    @resume = current_candidate&.resume || Resume.find(params[:id])
-  end
-
-  def edit; end
-
-  def update
-    @resume.update(resume_params)
-    redirect_to resume_path, notice: t('.success') if @resume.save
-  end
-
-  private
-
-  # rubocop:disable Naming/MemoizedInstanceVariableName
-  def set_resume
-    @resume ||= current_candidate.resume
-  end
-  # rubocop:enable Naming/MemoizedInstanceVariableName
-
-  def resume_params
-    params.require(:resume).permit(:address,
-                                   :education,
-                                   :experience,
-                                   :languages,
-                                   :abilities,
-                                   :courses)
+    @resume = Resume.find(params[:id])
   end
 end
--- spec/controllers/resumes_controller_spec.rb
+++ spec/controllers/resumes_controller_spec.rb
@@ -23,23 +23,4 @@ RSpec.describe ResumesController, type: :controller, login_metadata: true do
       end
     end
   end
-
-  describe '#edit', candidate_signed_in: true do
-    it 'renders template edit' do
-      get :edit
-
-      expect(response).to render_template('edit')
-      expect(assigns(:resume)).to eq resume
-    end
-  end
-
-  describe '#update', candidate_signed_in: true do
-    it 'updates resume and redirects to show' do
-      patch :update, params: { id: resume.id, resume: { address: 'Paraíba' } }
-
-      expect(resume.reload.address).to eq 'Paraíba'
-      expect(response).to redirect_to resume_path
-      expect(flash[:notice]).to be_present
-    end
-  end
 end
