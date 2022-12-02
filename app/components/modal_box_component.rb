b8b6df1e3214407a9fa0fe9352705d72d7fa3891
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-02 15:17:39 -0300

    modal appears when clicking on button

--- app/components/modal_box_component.rb
+++ app/components/modal_box_component.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class ModalBoxComponent < ViewComponent::Base
+  renders_one :body
 
+  # show_button_id: HTML element id
+  def initialize(title: '', show_button_id: '', modal_id: '')
+    @title = title
+    @show_button_id = show_button_id
+    @modal_id = modal_id
+    super
+  end
 end
