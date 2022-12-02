f8c88c2a2b277531cc86e7f4d209837d27f7a6e1
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-02 17:09:33 -0300

    test camdidates controller

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,9 +1,11 @@
 class CandidatesController < ApplicationController
-  before_action :authenticate_candidate!
+  before_action :authenticate_candidate!, only: :dashboard
 
   def dashboard; end
 
   def show
+    redirect_to root_path unless candidate_signed_in? || employee_signed_in?
+
     @candidate = Candidate.includes(:work_experiences).find(params[:id])
   end
 end
