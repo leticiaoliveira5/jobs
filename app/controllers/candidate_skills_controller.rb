23455cae78563bd743252efa3aad896b011b9e62
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 19:42:48 -0300

    try fix cc

--- app/controllers/candidate_skills_controller.rb
+++ app/controllers/candidate_skills_controller.rb
@@ -2,8 +2,7 @@ class CandidateSkillsController < ApplicationController
   before_action :authenticate_candidate!
 
   def create
-    @candidate_skill = current_candidate.candidate_skills.new(skill: find_or_create_skill,
-                                                              level: skill_params[:level].to_i)
+    @candidate_skill = build_candidate_skill
     @candidate_skill.save
     redirect_to candidate_path(current_candidate)
   end
@@ -16,10 +15,10 @@ class CandidateSkillsController < ApplicationController
 
   private
 
-  def find_or_create_skill
-    name_input = skill_params[:name]
-    name = name_input[0].capitalize + name_input[1..].downcase
-    Skill.find_or_create_by(name: name)
+  def build_candidate_skill
+    name = skill_params[:name]
+    current_candidate.candidate_skills.new(skill: Skill.find_or_create_by_name(name),
+                                           level: skill_params[:level].to_i)
   end
 
   def skill_params
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,4 +3,8 @@ class Skill < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  def self.find_or_create_by_name(name)
+    find_or_create_by(name: name[0].capitalize + name[1..].downcase)
+  end
 end
