2d366569772d14a6584651b0060fedb704d79269
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 20:25:22 -0300

    fix cc

--- app/helpers/candidate_profile_helper.rb
+++ app/helpers/candidate_profile_helper.rb
@@ -2,12 +2,12 @@ module CandidateProfileHelper
   def add_candidate_skill_button
     return unless candidate_signed_in?
 
-    content_tag(:button, '+', id: 'add_candidate_skill_button') 
+    tag.button('+', id: 'add_candidate_skill_button')
   end
 
   def delete_candidate_skill_button(skill)
     return unless candidate_signed_in?
-    
-    link_to content_tag(:button, 'trash_icon'), candidate_skill_path(skill.id), method: :delete 
+
+    link_to tag.button('trash_icon'), candidate_skill_path(skill.id), method: :delete
   end
 end
