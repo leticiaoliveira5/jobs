4440768846dc53668bf0c17f8beafa6191d822c6
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 16:17:58 -0300

    use helper correctly from controller

--- app/controllers/candidate_skills_controller.rb
+++ app/controllers/candidate_skills_controller.rb
@@ -2,9 +2,9 @@ class CandidateSkillsController < ApplicationController
   before_action :authenticate_candidate!
 
   def create
-    @find_or_create_skill = Skill.find_or_create_by(name: normalize_name(skill_params[:name]))
+    @skill = Skill.find_or_create_by(name: helpers.normalize_name(skill_params[:name]))
     @candidate_skill = current_candidate.candidate_skills.new(level: skill_params[:level])
-    @candidate_skill.skill = @find_or_create_skill
+    @candidate_skill.skill = @skill
     @candidate_skill.save
     redirect_to candidate_path(current_candidate)
   end
