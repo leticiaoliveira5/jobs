85501e6318d52bbe4dfa5b6f871419bf8a9816fe
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 19:10:43 -0300

    fix cc

--- app/models/candidate_skill.rb
+++ app/models/candidate_skill.rb
@@ -7,6 +7,8 @@ class CandidateSkill < ApplicationRecord
   enum level: { beginner: 0, intermediate: 1, advanced: 2 }
 
   def self.level_humanized_names
-    levels.map { |k, v| [I18n.t("activerecord.attributes.candidate_skill.level.#{k}"), v] }
+    levels.map do |key, value|
+      [I18n.t("activerecord.attributes.candidate_skill.level.#{key}"), value]
+    end
   end
 end
