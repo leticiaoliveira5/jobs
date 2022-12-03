227769409179824bbdbcc560d2169c5173bb72f2
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 16:43:45 -0300

    betterment

--- app/controllers/candidate_skills_controller.rb
+++ app/controllers/candidate_skills_controller.rb
@@ -2,7 +2,8 @@ class CandidateSkillsController < ApplicationController
   before_action :authenticate_candidate!
 
   def create
-    @skill = Skill.find_or_create_by(name: helpers.normalize_name(skill_params[:name]))
+    name = helpers.normalize_name(skill_params[:name])
+    @skill = Skill.find_or_create_by(name: name)
     @candidate_skill = current_candidate.candidate_skills.new(level: skill_params[:level])
     @candidate_skill.skill = @skill
     @candidate_skill.save
