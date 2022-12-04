21817c3286de1170c3f211225cb8e8fa12bad208
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-03 21:08:23 -0300

    add unit test

--- spec/models/candidate_skill_spec.rb
+++ spec/models/candidate_skill_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe CandidateSkill, type: :model do
     it { is_expected.to belong_to(:skill) }
   end
 
+  describe 'enum' do
+    it { is_expected.to define_enum_for(:level).with_values([:beginner, :intermediate, :advanced]) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_uniqueness_of(:skill_id).scoped_to(:candidate_id) }
+  end
+
   describe 'level_humanized_names' do
     it 'lists enum values' do
       expect(described_class.level_humanized_names)
