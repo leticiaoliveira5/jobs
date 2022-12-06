844ded316c2cc8036c8cc7d77c4bd0d2143b7922
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-06 14:30:52 -0300

    factory

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :course do
     title { 'Aesthetics of Chinese Plates' }
     candidate
+    description { 'My String' }
+    certificate_link { 'google.com' }
   end
 end
