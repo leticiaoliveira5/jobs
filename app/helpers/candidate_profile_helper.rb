66b75b7a099b2dfe14fbb840e87764653bcd216d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-06 14:04:04 -0300

    displays courses on candidate page

--- app/helpers/candidate_profile_helper.rb
+++ app/helpers/candidate_profile_helper.rb
@@ -1,4 +1,11 @@
 module CandidateProfileHelper
+
+  def add_item_button(id)
+    return unless candidate_signed_in?
+
+    tag.button('+', id: id)
+  end
+
   def add_candidate_skill_button
     return unless candidate_signed_in?
 
@@ -9,5 +16,27 @@ module CandidateProfileHelper
     return unless candidate_signed_in?
 
     link_to tag.button('trash_icon'), candidate_skill_path(skill.id), method: :delete
+  end
+
+  def add_course_button
+    return unless candidate_signed_in?
+
+    tag.button('+', id: 'add_course_button')
+  end
+
+  def delete_course_button(course_id)
+    return unless candidate_signed_in?
+
+    link_to tag.button('trash_icon'), course_path(course_id), method: :delete
+  end
+
+  def course_certificate_link(certificate_link)
+    return unless candidate_signed_in? && certificate_link
+
+    link_to('Certificate_icon',
+            "http://#{certificate_link}",
+            target: :_blank,
+            title: t('candidates.show.see_certificate'),
+            rel: :noopener).concat(tag.br)
   end
 end
