e9d1e94bba4fd5c840726f96da9783e3d9c84074
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-07 14:47:18 -0300

    remove enum test

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe Language, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:proficiency) }
   end
-
-  describe 'enum' do
-    it do
-      expect(subject).to define_enum_for(:proficiency)
-      .with_values(%i[beginner intermediate advanced fluent])
-    end
-  end
 end
