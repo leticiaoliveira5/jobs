db7bd9b07aa7ee31885f9ff2a8864e3ec071f6f7
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-07 15:44:14 -0300

    try to solve some issues

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,20 +1,21 @@
 class CandidatesController < ApplicationController
   before_action :authenticate_candidate!, only: :dashboard
+  before_action :authenticate_user, only: :show
 
   def dashboard; end
 
-  # This method smells of :reek:TooManyInstanceVariables
-  # This method smells of :reek:TooManyStatements
   def show
-    redirect_to root_path unless candidate_signed_in? || employee_signed_in?
-
-    @candidate = Candidate.includes(:work_experiences,
-                                    :candidate_skills,
-                                    :courses,
-                                    :languages).find(params[:id])
+    @candidate = Candidate.includes(:work_experiences, :candidate_skills,
+                                    :courses, :languages).find(params[:id])
     @work_experiences = @candidate.work_experiences.order(start_date: :desc)
     @candidate_skills = @candidate.candidate_skills
     @courses = @candidate.courses
     @languages = @candidate.languages
+  end
+
+  private
+
+  def authenticate_user
+    redirect_to root_path unless candidate_signed_in? || employee_signed_in?
   end
 end
