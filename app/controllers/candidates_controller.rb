72fb097bee1455a1ed88f9583b06529d3291a32a
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-12-07 16:28:44 -0300

    Adds Languages to Candidate Profile (#138)
    * generate languages
    
    * fix rubocop warnings
    
    * add translation
    
    * missing associaton in canidate model
    
    * routes and controller
    
    * fix rubocop
    
    * load candidate languages
    
    * add to profile
    
    * remove enum test
    
    * add coverage
    
    * ignore reek
    
    * try to solve some issues
    
    * change reek standards
    
    * undo
    
    * no instance variable lol

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,14 +1,20 @@
 class CandidatesController < ApplicationController
   before_action :authenticate_candidate!, only: :dashboard
+  before_action :authenticate_user, only: :show
 
   def dashboard; end
 
   def show
-    redirect_to root_path unless candidate_signed_in? || employee_signed_in?
-
-    @candidate = Candidate.includes(:work_experiences, :candidate_skills).find(params[:id])
+    @candidate = Candidate.includes(:work_experiences, :candidate_skills,
+                                    :courses, :languages).find(params[:id])
     @work_experiences = @candidate.work_experiences.order(start_date: :desc)
     @candidate_skills = @candidate.candidate_skills
     @courses = @candidate.courses
+  end
+
+  private
+
+  def authenticate_user
+    redirect_to root_path unless candidate_signed_in? || employee_signed_in?
   end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -10,6 +10,7 @@ class Candidate < ApplicationRecord
   has_many :candidate_skills, dependent: :destroy
   has_many :skills, through: :candidate_skills
   has_many :courses, dependent: :destroy
+  has_many :languages, dependent: :destroy
   has_one_attached :avatar
 
   validates :document, length: { is: 11 }, on: :update, allow_blank: true
