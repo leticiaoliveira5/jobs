152f2ce82f4ec442c22b021b0b5a775eb88dad1f
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-12 20:52:37 -0300

    refactor test

--- spec/factories/candidates.rb
+++ spec/factories/candidates.rb
@@ -16,5 +16,21 @@ FactoryBot.define do
       address { nil }
       about_me { nil }
     end
+
+    trait :with_avatar do
+      after(:build) do |candidate|
+        candidate.avatar.attach(io: File.open(Rails.root.join('spec/fixtures/img.jpg')),
+                                filename: 'img.jpg',
+                                content_type: 'image/jpeg')
+      end
+    end
+
+    trait :with_invalid_format_avatar do
+      after(:build) do |candidate|
+        candidate.avatar.attach(io: File.open(Rails.root.join('spec/fixtures/text.txt')),
+                                filename: 'text.txt',
+                                content_type: 'text/txt')
+      end
+    end
   end
 end
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -13,34 +13,21 @@ RSpec.describe Candidate, type: :model do
   describe 'validations' do
     it { is_expected.to validate_length_of(:document) }
 
+    #
     context 'avatar' do
-      let(:candidate) { build(:candidate) }
-
       it 'validates content type' do
-        file = Rails.root.join('spec/fixtures/text.txt')
-        txt = ActiveStorage::Blob.create_and_upload!(
-          io: File.open(file, 'rb'),
-          filename: 'text.txt',
-          content_type: 'text/txt'
-        ).signed_id
+        candidate = build(:candidate, :with_invalid_format_avatar)
 
-        candidate.avatar = txt
-        expect(candidate).not_to be_valid
+        expect(candidate).to be_invalid
         expect(candidate.errors).to include(:avatar)
       end
 
       it 'validates size' do
-        file = Rails.root.join('spec/fixtures/img.jpg')
-        img = ActiveStorage::Blob.create_and_upload!(
-          io: File.open(file, 'rb'),
-          filename: 'img.jpg',
-          content_type: 'image/jpeg'
-        ).signed_id
+        candidate = build(:candidate, :with_avatar)
 
         allow_any_instance_of(ActiveStorage::Blob).to receive(:byte_size).and_return(1.megabyte)
 
-        candidate.avatar = img
-        expect(candidate).not_to be_valid
+        expect(candidate).to be_invalid
         expect(candidate.errors).to include(:avatar)
       end
     end
