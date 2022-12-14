02d13a38c3d5bce7afe770caa144a3e49ff69b39
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-13 22:03:14 -0300

    change up page

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,7 +2,9 @@ class CandidatesController < ApplicationController
   before_action :authenticate_candidate!, only: :dashboard
   before_action :authenticate_user, only: :show
 
-  def dashboard; end
+  def dashboard
+    @candidate = current_candidate
+  end
 
   def show
     @candidate = Candidate.includes(:work_experiences, :candidate_skills,
