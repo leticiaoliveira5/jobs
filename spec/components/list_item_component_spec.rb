6a5d6504f790f71ac3a887f05f3b813f68237659
leticiaoliveira5 <leoliveira994@gmail.com>
2022-12-13 21:16:00 -0300

    update test

--- spec/components/list_item_component_spec.rb
+++ spec/components/list_item_component_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe ListItemComponent, type: :component do
   let(:rendered_component) { render_inline(described_class.new).to_html }
 
-  it 'renders the login component' do
-    expect(rendered_component).to have_css('div')
+  it 'renders the component' do
+    expect(rendered_component).to have_css('div.list-item')
+    expect(rendered_component).to have_css('span.side-button')
   end
 end
