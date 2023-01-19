9e0c531d88da75521f507b17f74f52766dff1cdd
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-01-19 14:23:24 -0300

    test coverage for box component

--- spec/components/box_component_spec.rb
+++ spec/components/box_component_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe BoxComponent, type: :component do
+  let(:subject) { described_class.new }
   let(:rendered_component) do
-    render_inline(described_class.new) do |component|
+    render_inline(subject) do |component|
       component.title { 'Title' }
       component.body { 'Body' }
     end.to_html
@@ -13,5 +14,21 @@ RSpec.describe BoxComponent, type: :component do
   it 'renders the component' do
     expect(rendered_component).to have_css('.title', text: 'Title')
     expect(rendered_component).to have_css('.content', text: 'Body')
+  end
+
+  context 'attribute color is not set' do
+    it { expect(rendered_component).to include("background-color:var(--content_blocks_color)") }
+  end
+
+  context 'color attribute is set to primary' do
+    let(:subject) { described_class.new(color: :primary) }
+
+    it { expect(rendered_component).to include("background-color:var(--content_blocks_color)") }
+  end
+
+  context 'color attribute is set to secondary' do
+    let(:subject) { described_class.new(color: :secondary) }
+
+    it { expect(rendered_component).to include("background-color:var(--nav_background_color)") }
   end
 end
