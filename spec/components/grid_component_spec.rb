902e005377b667b5149f388512f235d293e09a3a
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-01-20 20:01:48 -0300

    fix coverage

--- spec/components/grid_component_spec.rb
+++ spec/components/grid_component_spec.rb
@@ -1,23 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe GridComponent, type: :component do
-  subject { described_class.new(columns: 2, rows: 2) }
-
   let(:rendered_component) { render_inline(subject).to_html }
+
+  context 'builds css with attributes' do
+    subject { described_class.new(columns: 2, rows: 3, column_gap: 2, row_gap: 2) }
+
+    it { expect(rendered_component).to match('grid-template-columns: 1fr 1fr ') }
+    it { expect(rendered_component).to match('grid-template-rows: auto auto auto') }
+    it { expect(rendered_component).to match('row-gap: 2px') }
+    it { expect(rendered_component).to match('column-gap: 2px') }
+  end
+
+  context 'builds css with nil attributes' do
+    subject { described_class.new }
+
+    it { expect(rendered_component).to match('grid-template-columns: auto') }
+    it { expect(rendered_component).to match('grid-template-rows: auto') }
+    it { expect(rendered_component).to match('row-gap: 1rem') }
+    it { expect(rendered_component).to match('column-gap: 1rem') }
+  end
 
   context 'is responsive' do
     subject { described_class.new(type: :responsive) }
 
-    it 'renders the component' do
-      expect(rendered_component).to have_css('.responsive-grid')
-    end
+    it { expect(rendered_component).to have_css('.responsive-grid') }
   end
 
   context 'is fixed' do
     subject { described_class.new(type: :fixed) }
 
-    it 'renders the component' do
-      expect(rendered_component).to have_css('.grid')
-    end
+    it { expect(rendered_component).to have_css('.grid') }
   end
 end
