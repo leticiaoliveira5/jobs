c8b23add1e3eca6443c2fcf868d39f291cc678a5
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2023-01-21 12:53:22 -0300

    Revamps companies index (#164)
    * add search and componentize page
    
    * add includes for query
    
    * add translation
    
    * fix modal box
    
    * helper method
    
    * generate company preview component
    
    * use component
    
    * add transition to links
    
    * shows search results on index page
    
    * show input on results
    
    * add feature test
    
    * add feature test
    
    * refactor test
    
    * fix cc
    
    * fix cc
    
    * move css to css file
    
    * adds empty state
    
    * adjust view

---
+++ spec/features/visitor/visitor_search_companies_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Visitor searches job opportunities' do
+  before do
+    create(:company, name: 'Apple', address: 'California', domain: 'jobs.com')
+    visit root_path
+    click_on 'Empresas'
+  end
+
+  scenario 'by name' do
+    fill_in :search_input, with: 'app'
+    expect_to_find_company
+  end
+
+  scenario 'by address' do
+    fill_in :search_input, with: 'California'
+    expect_to_find_company
+  end
+
+  scenario 'by domain' do
+    fill_in :search_input, with: 'jobs'
+    expect_to_find_company
+  end
+
+  def expect_to_find_company
+    page.find('button[type="submit"]').click
+    expect(current_path).to eq(companies_path)
+    expect(page).to have_content('Resultado da Busca') && have_link('Apple')
+  end
+end
