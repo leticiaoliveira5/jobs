f61d10683e09e82a9267cfc7771695f06eb4f33b
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-01-21 14:51:04 -0300

    update test

--- spec/features/visitor/visitor_search_for_job_opportunity_spec.rb
+++ spec/features/visitor/visitor_search_for_job_opportunity_spec.rb
@@ -6,28 +6,26 @@ feature 'Visitor searches job opportunities' do
   before do
     create(:job_opportunity, company: apple, job_title: 'Desenvolvedor')
     create(:job_opportunity, company: apple, job_title: 'Supervisor')
-  end
-
-  scenario 'successfully' do
     visit root_path
     click_on 'Vagas'
+  end
+
+  scenario 'by job title' do
     fill_in :search_input, with: 'desenvolvedor'
     page.find('button[type="submit"]').click
 
-    expect(current_path).to eq(search_results_path)
-    expect(page).to have_link 'Desenvolvedor'
-    expect(page).to have_link 'Apple'
+    expect(current_path).to eq(job_opportunities_path)
+    expect(page).to have_text 'Desenvolvedor'
+    expect(page).to have_text 'Apple'
   end
 
   scenario 'by company name' do
-    visit root_path
-    click_on 'Vagas'
     fill_in :search_input, with: 'Apple'
     page.find('button[type="submit"]').click
 
-    expect(current_path).to eq(search_results_path)
-    expect(page).to have_link 'Desenvolvedor'
-    expect(page).to have_link 'Supervisor'
-    expect(page).to have_link 'Apple'
+    expect(current_path).to eq(job_opportunities_path)
+    expect(page).to have_text 'Desenvolvedor'
+    expect(page).to have_text 'Supervisor'
+    expect(page).to have_text 'Apple'
   end
 end
