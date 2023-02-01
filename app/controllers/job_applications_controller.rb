47f1e3ccf0efefa7cf31ddc19df941f1799ad056
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-02-01 20:54:44 -0300

    refactor ja controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,24 +1,29 @@
 class JobApplicationsController < ApplicationController
+  before_action :set_job_application, :set_candidate
   before_action :authenticate_candidate!, only: %i[destroy]
   before_action :authenticate_employee!, only: %i[decline]
 
-  def show
-    @job_application = JobApplication.find(params[:id])
-    @candidate = @job_application.candidate
-  end
+  def show; end
 
   def destroy
-    @job_application = JobApplication.find(params[:id])
-    @candidate = @job_application.candidate
     @job_application.destroy
     redirect_to '/candidate/dashboard', notice: t('.success')
   end
 
   def decline
-    @job_application = JobApplication.find(params[:id])
     @job_application.rejection_motive = params[:rejection_motive]
     @job_application.declined!
     redirect_to company_path(@job_application.company),
-                notice: t('.success', candidate: @job_application.candidate.firstname)
+                notice: t('.success', candidate: @candidate.firstname)
+  end
+
+  private
+
+  def set_job_application
+    @job_application = JobApplication.find(params[:id])
+  end
+
+  def set_candidate
+    @candidate = @job_application.candidate
   end
 end
