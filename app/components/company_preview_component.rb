b8f209982b8928873dfe0115c28ad6d8b7b3d85f
leticiaoliveira5 <olivaleticia@hotmail.com>
2025-02-17 13:09:21 -0300

    adjust company display

--- app/components/company_preview_component.rb
+++ app/components/company_preview_component.rb
@@ -4,10 +4,9 @@ class CompanyPreviewComponent < ViewComponent::Base
   include ApplicationHelper
   include FontAwesome::Rails::IconHelper
 
-  def initialize(type: :responsive, title: nil, title_link: nil, see_more: nil)
+  def initialize(type: :responsive, title: nil, title_link: nil)
     super
     @title = title
     @title_link = title_link
-    @see_more = see_more
   end
 end
